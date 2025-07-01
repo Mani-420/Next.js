8041b8a001bb8b91dabbc467676d3e3188ac9ac3
Mani-420 <tahirabdulrehman46@gmail.com>
2025-07-01 23:38:38 +0500

    Schemas

---
+++ nextjs/src/schemas/signUpSchema.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod';
+
+export const usernameValidation = z
+  .string()
+  .min(3, 'Username must be at least 3 characters long')
+  .max(20, 'Username must be at most 20 characters long')
+  .regex(
+    /^[a-zA-Z0-9_]+$/,
+    'Username can only contain letters, numbers, and underscores'
+  );
+
+export const signUpSchema = z.object({
+  username: usernameValidation,
+  email: z
+    .string()
+    .email({ message: 'Invalid email address' })
+    .min(5, 'Email must be at least 5 characters long'),
+  password: z
+    .string()
+    .min(6, { message: 'Password must be at least 6 characters long' })
+});
